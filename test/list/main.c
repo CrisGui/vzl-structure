5b42d691ef877539c1e59a54491542716e235cea
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-11-18 18:58:13 +0000

    test: List constructor and destructor

--- test/list/main.c
+++ test/list/main.c
@@ -1,7 +1,25 @@
 #include "../../source/list/list.h"
+#include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
+  struct dummy {
+    char name[10];
+    unsigned int age;
+    unsigned long crashes;
+    _Bool alive;
+  } my_dummy = {.name = "Tommy DJ", .age = 83, .crashes = 23352, .alive = 1};
+
+  /*Constructor*/
+  list_t *my_list = list_create(sizeof(int));
+  assert(my_list != NULL);
+
+  /*Destructor*/
+  assert(0 == list_destroy(&my_list));
+  assert(NULL == my_list);
+
   printf("[LIST]: All tests ran successfully\n");
   return 0;
 }
